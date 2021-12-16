cd5ea2e75a6c4128436101ea961d330dd96aa13b
Akib Ali <akib7599@gmail.com>
2021-12-16 13:18:24 +0530

    Slidingbar for landing page

---
+++ slidingbar/slider.js
@@ -0,0 +1,16 @@
+$(".slider").owlCarousel({
+    margin:20,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items: 1,
+            nav:false
+        }
+      
+        }
+
+    
+  })
