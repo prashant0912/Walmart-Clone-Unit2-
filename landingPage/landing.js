c36896bea253a6cd37951d701d94410618764183
Shoaib Ahmed <sahmed094@gmail.com>
2021-12-16 23:06:33 +0530

    merge i add popup cart working properly

--- landingPage/landing.js
+++ landingPage/landing.js
@@ -13,7 +13,35 @@ document.addEventListener("click", e => {
       dropdown.classList.remove("active")
     })
   })
+// cart popup window function for appearing and disappearing add by shoaib (16th dec 9:45pm)
 
+    function popup()
+    {
+       let change = document.getElementById("popup");
+       change.style.display = "block";
+    }
+    function popuphide()
+    {
+       let change = document.getElementById("popup");
+       change.style.display = "none";
+    }
+
+    function gotoGroceryPage()
+    {
+      window.location.href="https://www.walmart.com/cp/food/976759";
+    }
+    function gotoElectronicsPage()  /* replace with ankit electronic page link */
+    {
+      window.location.href="https://www.walmart.com/cp/electronics/3944"
+    }
+    function gotolandingPage(){
+      window.location.href="http://127.0.0.1:5500/Walmart-Clone-Unit2-/landingPage/landing.html";
+    }
+    function goToToys(){
+      window.location.href="https://www.walmart.com/cp/toys/4171";
+    }
+
+    // popup window code fucntion end here (shoaib)
 
 
   // sliding-JavaScript start---->
