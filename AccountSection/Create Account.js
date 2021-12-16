36c3d4167d8a3fce4b9899bad46878501953ddbe
DEVESH VERMA <71995793+webdeveshverma@users.noreply.github.com>
2021-12-16 11:10:08 +0530

    add js fiel

---
+++ AccountSection/Create Account.js
@@ -0,0 +1,21 @@
+document.querySelector("#Create_Account").addEventListener("click", createAccount)
+var userData = [] || JSON.parse(localStorage.getItem("userDatabase"));
+
+function createAccount() {
+    var fname = document.querySelector("#fname").value;
+    var lname = document.querySelector("#lname").value;
+    var email = document.querySelector("#email").value;
+    var password = document.querySelector("#password").value;
+
+
+    var userdetails = {
+        first_name: fname,
+        last_name: lname,
+        email_Id: email,
+        password: password
+    };
+
+    userData.push(userdetails);
+    localStorage.setItem("userDatabase", JSON.stringify(userData));
+
+}
