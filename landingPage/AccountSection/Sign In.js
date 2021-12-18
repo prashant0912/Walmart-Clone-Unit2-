db19a8881a53bbbf09059f2de71dbbbcec135297
DEVESH VERMA <71995793+webdeveshverma@users.noreply.github.com>
2021-12-18 09:49:17 +0530

    Update Sign In.js

--- landingPage/AccountSection/Sign In.js
+++ landingPage/AccountSection/Sign In.js
@@ -14,7 +14,7 @@ else {
     if (regdUsers[i].email_Id == email && regdUsers[i].password == password) {
       window.location.href = "\Walmart-Clone-Unit2-\landingPage\landing.html" ;
         alert("User Login Successful")
-       window.location.href = "";
+       window.location.href = "landingPage/landing.html";
     }
   
   }
