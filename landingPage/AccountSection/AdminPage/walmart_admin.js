81b8ce3e7950c13971c533d81becd807f183dce7
Shoaib Ahmed <sahmed094@gmail.com>
2021-12-19 20:35:20 +0530

    changes

--- landingPage/AccountSection/AdminPage/walmart_admin.js
+++ landingPage/AccountSection/AdminPage/walmart_admin.js
@@ -1,14 +1,16 @@
 document.getElementById('adminform').addEventListener("submit",addProducts);
-let arr =[];
+let arr = JSON.parse(localStorage.getItem("addProduct"))||[];
 function addProducts(event){
     event.preventDefault();
 
     var addItem ={
+        deal:adminform.dil.value,
         img_url:adminform.url.value,
-        cat: adminform.cat.value,
-        Prod_name: adminform.name.value,
-        description:adminform.des.value,
-        price:adminform.price.value,
+        price: adminform.price.value,
+        actual:adminform.actual.value,
+        describe: adminform.des.value,
+        feature:adminform.feature.value,
+        shipping:adminform.ship.value,
     }
 
     arr.push(addItem);
@@ -19,6 +21,5 @@ function addProducts(event){
 
 function clearEntry()
 {
-    // event.preventDefault();
-    window.location.href="walmart_admi.html";
+    let azx =JSON.parse(localStorage.getItem("addProduct"))
 }
--- landingPage/landing.js
+++ landingPage/landing.js
@@ -96,3 +96,284 @@ function shopCovidVaccine()
        window.location.href ="https://dashboard.cowin.gov.in/"
    }
 // js code for vaccine column end
+
+
+
+// all code which affect functionality of the cart
+
+
+
+var data =JSON.parse(localStorage.getItem('addProduct'));  //fetch data from storage to which is inpu by product add and here we map this in landing page
+    var cartarr =JSON.parse(localStorage.getItem('walmartCart'))||[]; //fetching add to cart data
+    // var cartarr =[];
+    document.querySelector("#lalala>#icount").textContent = cartarr.length;
+
+displayItem(data); //display added product
+
+// function handlePriceSort(){  //function to catch the select tag values
+//   let sel =document.querySelector("#priceSort").value;
+//   // console.log(sel);
+//   if(sel=="low")
+//   {
+//       data.sort(function(a,b){
+//           let x = Number(a.price) - Number(b.price);
+//           return x;
+//       })
+//   }
+//    if(sel=="high")
+//   {
+//       data.sort(function(a,b){
+//           let x = Number(b.price) - Number(a.price);
+//           return x;
+//       })
+//   }
+//   // console.log(data) //to check working of sort
+//   displayItem(data)
+// }
+
+// function handleNameSort(){
+//   let sort =document.getElementById("nameSort").value;
+
+//   if(sort =="asc")
+//   {
+//       data.sort(function(a,b)
+//       {
+//           if(a.name > b.name)
+//           {
+//               return 1;
+//           }
+//           if(a.name <b.name)
+//           {
+//               return -1;
+//           }
+//           return 0;
+//       })
+//   }
+//   if(sort =="dse")
+//   {
+//       data.sort(function (a,b){
+          
+//           if(a.name > b.name)
+//           {
+//               return -1;
+//           }
+//           if(a.name <b.name)
+//           {
+//               return 1;
+//           }
+//           return 0;
+//       })
+//   }
+//   // console.log(data)
+//   displayItem(data);
+// }
+
+
+function displayItem(data){
+
+
+
+  document.getElementById("cartItemShoaib").innerHTML = "";
+
+  data.map(function (elem, index) {  //use to map data
+      
+      var div = document.createElement('div')
+      div.setAttribute('id',"bXart")
+      
+      var s1 =document.createElement("span") 
+      s1.setAttribute("id","deal1")
+      s1.innerHTML =elem.deal
+
+      var d1 =document.createElement("div")
+      d1.setAttribute("id","imgbox1") //c above
+
+      var img1 =document.createElement("img")
+      img1.setAttribute("src", elem.img_url)
+
+      var d2 =document.createElement("div")
+      d2.setAttribute("id","append") // next to span append in this div
+
+      var s2 =document.createElement("span")
+      s2.setAttribute("id","price1")
+      s2.innerHTML = elem.price;
+
+      var s3 = document.createElement("span")
+      s3.setAttribute("id","realPrice1")
+      s3.innerHTML=elem.actual;
+
+      var but =document.createElement("button")
+      but.innerHTML ="Cart+"
+          but.addEventListener("click",function()
+          { // button use to add to cart by localstoring and maping
+              
+              cartarr.push(elem)
+              
+              window.location.href="landing.html"
+              localStorage.setItem("walmartCart",JSON.stringify(cartarr));
+              showpopupvalue()
+            //   document.querySelector("#chuparustam").style.display.block;
+          }) 
+
+      var p1 =document.createElement("p")
+      p1.setAttribute("id","dtl")
+      p1.innerHTML=elem.describe;
+
+      var p2 =document.createElement("p")
+      p2.setAttribute("id","fetch")
+      p2.innerHTML =elem.feature;
+
+      var s4 =document.createElement("span")
+      s4.setAttribute("id","shipping1")
+      s4.innerHTML =elem.shipping;
+
+      d1.append(img1) //append in div laterS
+
+      d2.append(s2,s3) //append in div later
+        
+      var b =document.createAttribute("br")
+      div.append(s1,d1,d2,but,p1,p2)
+    //   console.log(div)
+      document.querySelector("#cartItemShoaib").append(div)
+  })
+}
+
+var cartmap1 =JSON.parse(localStorage.getItem("walmartCart"))||[]; 
+    // var cartmap1 =[];
+    let arrsum =[];
+
+    display(cartmap1);
+
+    function display(cartmap1)
+    {
+        document.querySelector("#tobebuy").innerHTML ="";
+        cartmap1.map(function (elem,index){
+            var div = document.createElement('div')
+            div.setAttribute('id',"bXart1")
+            
+            var s1 =document.createElement("span") 
+            s1.setAttribute("id","deal12")
+            s1.innerHTML =elem.deal
+
+            var d1 =document.createElement("div")
+            d1.setAttribute("id","imgbox12") //c above
+
+            var img1 =document.createElement("img")
+            img1.setAttribute("src", elem.img_url)
+
+            var d2 =document.createElement("div")
+            d2.setAttribute("id","append1") // next to span append in this div
+
+            var s2 =document.createElement("span")
+            s2.setAttribute("id","price12")
+            s2.innerHTML = elem.price;
+
+            var s3 = document.createElement("span")
+            s3.setAttribute("id","realPrice12")
+            s3.innerHTML=elem.actual;
+
+            var but =document.createElement("button")
+            but.innerHTML ="Remove"
+        
+            but.addEventListener("click", function ()
+            {
+                document.querySelector("#tobebuy").innerHTML ="";
+                cartmap1.splice(index,1);
+
+                window.location.href="landing.html"
+
+                localStorage.setItem("walmartCart",JSON.stringify(cartmap1))
+                let del = JSON.parse(localStorage.getItem("walmartCart"))
+                del.map(function (elem,index) {
+
+                    var car =JSON.parse(localStorage.getItem('walmartCart'))||[];
+                    
+                    var div = document.createElement('div')
+                            div.setAttribute('id',"bXart1")
+                            
+                            var s1 =document.createElement("span") 
+                            s1.setAttribute("id","deal12")
+                            s1.innerHTML =elem.deal
+
+                            var d1 =document.createElement("div")
+                            d1.setAttribute("id","imgbox12") //c above
+
+                            var img1 =document.createElement("img")
+                            img1.setAttribute("src", elem.img_url)
+
+                            var d2 =document.createElement("div")
+                            d2.setAttribute("id","append") // next to span append in this div
+
+                            var s2 =document.createElement("span")
+                            s2.setAttribute("id","price12")
+                            s2.innerHTML = elem.price;
+
+                            var s3 = document.createElement("span")
+                            s3.setAttribute("id","realPrice1")
+                            s3.innerHTML=elem.actual;
+
+                            var but =document.createElement("button")
+                            but.innerHTML ="Remove"
+                            but.addEventListener("click",function()
+                            {
+                                let sum =0;
+                                sum += +(elem.price)
+                                arrsum.push(sum)
+                                window.location.href="landing.html"
+                                // cartmap1.splice(index,1);
+                                // console.log(cartmap1)
+                                // localStorage.setItem("walmartCart",JSON.stringify(del))
+                                
+                            }) 
+                            var p1 =document.createElement("p")
+                            p1.setAttribute("id","dtl")
+                            p1.innerHTML=elem.describe;
+                      
+                            var p2 =document.createElement("p")
+                            p2.setAttribute("id","fetch1")
+                            p2.innerHTML =elem.feature;
+                      
+                            var s4 =document.createElement("span")
+                            s4.setAttribute("id","shipping12")
+                            s4.innerHTML =elem.shipping;
+                      
+                            d1.append(img1) //append in div laterS
+                      
+                            d2.append(s2,s3) //append in div later
+                              
+                            // var b =document.createAttribute("br")
+                            div.append(s1,d1,d2,but,p1,p2)
+                            document.querySelector("tobebuy").append(div)
+                        document.querySelector("#lalala>#icount").textContent = car.length;
+                        display(cartmap1)
+                })
+            }) 
+            var p1 =document.createElement("p")
+            p1.setAttribute("id","dtl1")
+            p1.innerHTML=elem.describe;
+
+            var p2 =document.createElement("p")
+            p2.setAttribute("id","fetch1")
+            p2.innerHTML =elem.feature;
+
+            var s4 =document.createElement("span")
+            s4.setAttribute("id","shipping12")
+            s4.innerHTML =elem.shipping;
+
+            d1.append(img1) //append in div laterS
+
+            d2.append(s2,s3) //append in div later
+                
+            // var b =document.createAttribute("br")
+            div.append(s1,d1,d2,but,p1,p2)
+            document.querySelector("#tobebuy").append(div)
+        })
+    }
+    function showpopupvalue()
+    {
+      document.querySelector("#disappear").style.display="none";
+    }
+
+    function abrakadabra()
+    {
+        window.location.href ="/paymentcard/card.html";
+    }
